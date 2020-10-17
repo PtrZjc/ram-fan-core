55e1f580ba33593ec7e6db43dbca2c36838ad369
ptr.zjc@gmail.com <ptr.zjc@gmail.com>
2020-10-17 22:37:24 +0200

    Initialized spring boot app commit

---
+++ src/main/java/pl/zajacp/ramfancore/RamFanCoreApplication.java
@@ -0,0 +1,13 @@
+package pl.zajacp.ramfancore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamFanCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamFanCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/zajacp/ramfancore/RamFanCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zajacp.ramfancore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamFanCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
