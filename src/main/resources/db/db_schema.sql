f3d8b2c35dc21579301ce9b27c8b01361a612148
Zajac P <ptr.zjc@gmail.com>
2020-10-21 22:18:08 +0200

    Added liquibase changelog config

--- src/main/resources/db/db_schema.sql
+++ src/main/resources/db/db_schema.sql
@@ -24,13 +24,13 @@ CREATE TABLE IF NOT EXISTS public.location (
     dimension           TEXT
 );
 
-CREATE TABLE IF NOT EXISTS public.character_episode ()
+CREATE TABLE IF NOT EXISTS public.character_episode (
     character_id        SMALLINT        NOT NULL        REFERENCES public.character,
     episode_id          SMALLINT        NOT NULL        REFERENCES public.episode,
     PRIMARY KEY (character_id, episode_id)
 );
 
-CREATE TABLE IF NOT EXISTS public.character_location ()
+CREATE TABLE IF NOT EXISTS public.character_location (
     character_id        SMALLINT        NOT NULL        REFERENCES public.character,
     location_id         SMALLINT        NOT NULL        REFERENCES public.location,
     PRIMARY KEY (character_id, location_id)
