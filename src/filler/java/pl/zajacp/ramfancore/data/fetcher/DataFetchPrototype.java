2b28851d250caa4cc258e3f6ae04332ca9a7cc63
Zajac P <ptr.zjc@gmail.com>
2020-11-15 16:11:32 +0100

    Finish prototype of fetching character data

--- src/filler/java/pl/zajacp/ramfancore/data/fetcher/DataFetchPrototype.java
+++ src/filler/java/pl/zajacp/ramfancore/data/fetcher/DataFetchPrototype.java
@@ -1,8 +1,11 @@
 package pl.zajacp.ramfancore.data.fetcher;
 
 import com.jayway.jsonpath.JsonPath;
+import org.jooq.DSLContext;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.RestTemplate;
 import pl.zajacp.ramfancore.data.fetcher.model.CharacterDto;
 
@@ -10,17 +13,23 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
+import static org.jooq.codegen.maven.example.tables.Character.CHARACTER;
+
+@SpringBootTest
 class DataFetchPrototype {
 
+    @Autowired
+    private DSLContext jooq;
+
     private final RestTemplate restTemplate = new RestTemplate();
-    private String link = "https://rickandmortyapi.com/api/character";
+    private final String LINK = "https://rickandmortyapi.com/api/character";
 
     @Test
     void prototype() throws JSONException {
 
-        Map<Integer, List<Integer>> characterToEpisodeRelations = new HashMap<>();
+//        Map<Integer, List<Integer>> characterToEpisodeRelations = new HashMap<>();
 
-        var nextPage = link;
+        var nextPage = LINK;
         var pageNum = 0;
         var pages = 0;
         do {
@@ -33,33 +42,44 @@ class DataFetchPrototype {
             List<CharacterDto> characters = new ArrayList<>();
 
             LongStream.range(0, idListAtPage.size()).boxed()
-                    .forEach(i -> {
-                        characters.add(CharacterDto.builder()
-                                .id((Integer) getResultObjectValue(response, i, "id"))
-                                .name((String) getResultObjectValue(response, i, "name"))
-                                .status((String) getResultObjectValue(response, i, "status"))
-                                .species((String) getResultObjectValue(response, i, "species"))
-                                .type((String) getResultObjectValue(response, i, "type"))
-                                .gender((String) getResultObjectValue(response, i, "gender"))
-                                .originId(getIdFromUrl((String) getResultObjectValue(response, i, "origin.url")))
-                                .locationId(getIdFromUrl((String) getResultObjectValue(response, i, "location.url")))
-                                .episodeIds(((List<String>) getResultObjectValue(response, i, "episode"))
-                                        .stream()
-                                        .map(this::getIdFromUrl)
-                                        .filter(Optional::isPresent)
-                                        .map(Optional::get)
-                                        .collect(Collectors.toList()))
-                                .imageBytes(restTemplate.getForObject((String) getResultObjectValue(response, i, "image"), byte[].class))
-                                .build());
-                    });
+                    .forEach(i -> characters.add(getCharacterDto(i, response)));
 
-            //                        characterToEpisodeRelations.put(character.getId(), character.getEpisodeIds());
+            characters.forEach(this::insertCharactersIntoDb);
 
+            //TODO - relations
+            //characterToEpisodeRelations.put(character.getId(), character.getEpisodeIds());
 
             System.out.println(characters);
             pageNum++;
-        } while (pageNum < 3);
-//        } while (nextPage != null);
+        } while (nextPage != null);
+    }
+
+    private Optional<Integer> getIdFromUrl(String url) {
+        if (!"".equals(url)) {
+            String[] segments = url.split("/");
+            return Optional.of(Integer.parseInt(segments[segments.length - 1]));
+        }
+        return Optional.empty();
+    }
+
+    private CharacterDto getCharacterDto(Long i, String response) {
+        return CharacterDto.builder()
+                .id((Integer) getResultObjectValue(response, i, "id"))
+                .name((String) getResultObjectValue(response, i, "name"))
+                .status((String) getResultObjectValue(response, i, "status"))
+                .species((String) getResultObjectValue(response, i, "species"))
+                .type((String) getResultObjectValue(response, i, "type"))
+                .gender((String) getResultObjectValue(response, i, "gender"))
+                .originId(getIdFromUrl((String) getResultObjectValue(response, i, "origin.url")))
+                .locationId(getIdFromUrl((String) getResultObjectValue(response, i, "location.url")))
+                .episodeIds(((List<String>) getResultObjectValue(response, i, "episode"))
+                        .stream()
+                        .map(this::getIdFromUrl)
+                        .filter(Optional::isPresent)
+                        .map(Optional::get)
+                        .collect(Collectors.toList()))
+                .imageBytes(restTemplate.getForObject((String) getResultObjectValue(response, i, "image"), byte[].class))
+                .build();
     }
 
     private Object getResultObjectValue(String json, Long index, String paramName) {
@@ -71,11 +91,17 @@ class DataFetchPrototype {
                 .toString());
     }
 
-    private Optional<Integer> getIdFromUrl(String url) {
-        if (!"".equals(url)) {
-            String[] segments = url.split("/");
-            return Optional.of(Integer.parseInt(segments[segments.length - 1]));
-        }
-        return Optional.empty();
+    private void insertCharactersIntoDb(CharacterDto characterDto) {
+        jooq.insertInto(CHARACTER)
+                .set(CHARACTER.ID, characterDto.getId())
+                .set(CHARACTER.NAME, characterDto.getName())
+                .set(CHARACTER.STATUS, characterDto.getStatus())
+                .set(CHARACTER.SPECIES, characterDto.getSpecies())
+                .set(CHARACTER.TYPE, characterDto.getType())
+                .set(CHARACTER.GENDER, characterDto.getGender())
+                .set(CHARACTER.ORIGIN_ID, characterDto.getOriginId().orElseGet(() -> null))
+                .set(CHARACTER.LOCATION_ID, characterDto.getLocationId().orElseGet(() -> null))
+                .set(CHARACTER.IMAGE, characterDto.getImageBytes())
+                .execute();
     }
 }
