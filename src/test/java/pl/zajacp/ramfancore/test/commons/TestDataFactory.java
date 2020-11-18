44da0f21aa030db2b9d0e265437d91be9f11014d
Zajac P <ptr.zjc@gmail.com>
2020-11-18 20:52:43 +0100

    Add TestData method to read images as bytes

--- src/test/java/pl/zajacp/ramfancore/test/commons/TestDataFactory.java
+++ src/test/java/pl/zajacp/ramfancore/test/commons/TestDataFactory.java
@@ -1,34 +1,64 @@
 package pl.zajacp.ramfancore.test.commons;
 
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
 import io.vavr.control.Try;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import static java.lang.String.join;
+import static java.nio.file.Files.readAllBytes;
 import static java.nio.file.Files.readAllLines;
+import static pl.zajacp.ramfancore.test.commons.TestDataFactory.DataPath.JPG_CHARACTER_IMAGE_FOLDER;
 
 public class TestDataFactory {
 
     @Getter
     @AllArgsConstructor
-    public enum DataFile {
+    public enum DataPath {
         API_CHARACTER_PAGE_1("target/test-classes/data/ext/character-page1.json"),
         API_CHARACTER_PAGE_2("target/test-classes/data/ext/character-page2.json"),
         API_EPISODE_PAGE_1("target/test-classes/data/ext/episode-page1.json"),
         API_EPISODE_PAGE_2("target/test-classes/data/ext/episode-page2.json"),
         API_LOCATION_PAGE_1("target/test-classes/data/ext/location-page1.json"),
         API_LOCATION_PAGE_2("target/test-classes/data/ext/location-page2.json"),
-        SQL_INSERT_30_CHARACTERS("target/test-classes/data/sql/insert-30-characters.sql");
+        SQL_INSERT_30_CHARACTERS("target/test-classes/data/sql/insert-30-characters.sql"),
+        JPG_CHARACTER_IMAGE_FOLDER("target/test-classes/data/image/character/");
+
         private String filePath;
     }
 
-    public static String getContent(DataFile dataFile) {
-        return contentOfFile(dataFile.getFilePath());
+    public static String getContent(DataPath dataPath) {
+        return contentOfFile(dataPath.getFilePath());
     }
 
     private static String contentOfFile(String path) {
-        return Try.of(() -> String.join("\n", readAllLines(Paths.get(path))))
+        return Try.of(() -> join("\n", readAllLines(Path.of(path))))
+                .getOrElseThrow((Supplier<RuntimeException>) RuntimeException::new);
+    }
+
+    public static List<Tuple2<Integer, byte[]>> getCharacterImagesAsBytes(int lowerIdBound, int upperIdBoundIncl) {
+        return IntStream.rangeClosed(lowerIdBound, upperIdBoundIncl).boxed()
+                .map(TestDataFactory::getCharacterImage)
+                .collect(Collectors.toList());
+    }
+
+    public static byte[] getCharacterImageAsBytes(int characterId) {
+        return getByteArrayFromFile(JPG_CHARACTER_IMAGE_FOLDER.getFilePath() + characterId + ".jpg");
+    }
+
+    private static Tuple2<Integer, byte[]> getCharacterImage(int characterId) {
+        return Tuple.of(characterId, getCharacterImageAsBytes(characterId));
+    }
+
+    private static byte[] getByteArrayFromFile(String path) {
+        return Try.of(() -> readAllBytes(Paths.get(path)))
                 .getOrElseThrow((Supplier<RuntimeException>) RuntimeException::new);
     }
 }
