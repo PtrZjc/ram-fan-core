02b819a3940acb8dee9c8e59ffefcb8812a7eb19
Zajac P <ptr.zjc@gmail.com>
2020-11-22 13:36:40 +0100

    Add restTemplate bean with configured root url

--- src/main/java/pl/zajacp/ramfancore/infrastructure/api/ApiConfig.java
+++ src/main/java/pl/zajacp/ramfancore/infrastructure/api/ApiConfig.java
@@ -1,15 +1,29 @@
 package pl.zajacp.ramfancore.infrastructure.api;
 
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ApiConfig {
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder){
-        return builder.build();
+    public RestTemplate restTemplate(RestTemplateBuilder builder, RamExternalPath ramExternalPath) {
+        return builder
+                .rootUri(ramExternalPath.rootUrl)
+                .build();
+    }
+
+    @Component
+    @EnableConfigurationProperties
+    @ConfigurationProperties(prefix = "api.ext")
+    @Setter
+    private class RamExternalPath {
+        private String rootUrl;
     }
 }
