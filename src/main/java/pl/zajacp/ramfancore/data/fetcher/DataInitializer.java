ba8a02914f16a2dfea9e3237c2a7069e9ab30971
Zajac P <ptr.zjc@gmail.com>
2020-11-29 21:17:03 +0100

    Invoke character fetch in data initializer

--- src/main/java/pl/zajacp/ramfancore/data/fetcher/DataInitializer.java
+++ src/main/java/pl/zajacp/ramfancore/data/fetcher/DataInitializer.java
@@ -1,14 +1,21 @@
 package pl.zajacp.ramfancore.data.fetcher;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
+@Slf4j
 public class DataInitializer {
+
+    private final CharacterFetcher characterFetcher;
 
     @EventListener
     public void onApplicationEvent(ContextRefreshedEvent event) {
-            System.out.println("here I will trigger fetching char/episode/location and set up the relations");
+        log.info("Initializing characters in the database");
+        characterFetcher.fetchDataAndSaveInDb();
     }
 }
