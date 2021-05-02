0dcb4fa66a4306c9ac82467ac76753e651d64d96
Zajac P <ptr.zjc@gmail.com>
2021-05-02 14:59:34 +0200

    Complete unit test for Episode data fetcher

--- src/test/java/pl/zajacp/ramfancore/test/commons/EpisodeTestRepository.java
+++ src/test/java/pl/zajacp/ramfancore/test/commons/EpisodeTestRepository.java
@@ -1,47 +1,28 @@
 package pl.zajacp.ramfancore.test.commons;
 
-import io.vavr.Tuple2;
 import org.jooq.DSLContext;
-import org.jooq.UpdateConditionStep;
-import org.jooq.impl.TableImpl;
-import pl.zajacp.ramfancore.model.tables.records.CharacterRecord;
 
-import static java.lang.Long.valueOf;
-import static java.util.stream.Collectors.toList;
-import static pl.zajacp.ramfancore.model.tables.Character.CHARACTER;
-import static pl.zajacp.ramfancore.test.commons.TestDataFactory.DataPath.SQL_INSERT_30_CHARACTERS;
-import static pl.zajacp.ramfancore.test.commons.TestDataFactory.getCharacterImagesAsBytes;
+import static pl.zajacp.ramfancore.model.tables.Episode.EPISODE;
+import static pl.zajacp.ramfancore.test.commons.TestDataFactory.DataPath.SQL_INSERT_30_EPISODES;
 import static pl.zajacp.ramfancore.test.commons.TestDataFactory.getContent;
 
-public class CharacterTestRepository extends AbstractTestDataRepository {
+public class EpisodeTestRepository extends AbstractTestDataRepository {
 
-    public CharacterTestRepository(DSLContext jooq) {
-        super(jooq, CHARACTER);
+    public EpisodeTestRepository(DSLContext jooq) {
+        super(jooq, EPISODE);
     }
 
     public void insertData(long lowerIdBound, long upperIdBoundIncl) {
         assert lowerIdBound > 0 && lowerIdBound < 30;
         assert upperIdBoundIncl > 1 && upperIdBoundIncl <= 30;
 
-        jooq.execute(getContent(SQL_INSERT_30_CHARACTERS));
-        jooq.batch(
-                getCharacterImagesAsBytes(lowerIdBound, upperIdBoundIncl)
-                        .stream()
-                        .map(this::prepareSingleImageUpdate)
-                        .collect(toList()))
-                .execute();
+        jooq.execute(getContent(SQL_INSERT_30_EPISODES));
 
         if (lowerIdBound > 1) {
-            jooq.delete(CHARACTER).where(CHARACTER.ID.between(1L, lowerIdBound - 1)).execute();
+            jooq.delete(EPISODE).where(EPISODE.ID.between(1L, lowerIdBound - 1)).execute();
         }
         if (upperIdBoundIncl < 30) {
-            jooq.delete(CHARACTER).where(CHARACTER.ID.between(upperIdBoundIncl + 1, 30L)).execute();
+            jooq.delete(EPISODE).where(EPISODE.ID.between(upperIdBoundIncl + 1, 30L)).execute();
         }
-    }
-
-    private UpdateConditionStep<CharacterRecord> prepareSingleImageUpdate(Tuple2<Long, byte[]> characterImage) {
-        return jooq.update(CHARACTER)
-                .set(CHARACTER.IMAGE, characterImage._2)
-                .where(CHARACTER.ID.eq(valueOf(characterImage._1)));
     }
 }
